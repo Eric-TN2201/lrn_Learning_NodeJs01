659ebe24e80dbbef10371e21e4b739b04644b014
Eric-TN2201 <trongnghia.eric@gmail.com>
2024-12-08 17:11:43 +0700

    add countries db

--- index.js
+++ index.js
@@ -1,7 +1,6 @@
 import express from "express";
 import bodyParser from "body-parser";
 import pg from "pg";
-import { render } from "ejs";
 
 const app = express();
 const port = 3000;
@@ -19,15 +18,34 @@ db.connect();
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static("public"));
 
-app.get("/", async (req, res) => {
-  const result = await db.query("select * from visited_countries");
+async function checkVisitedCountry(){
+  const result = await db.query("select country_code from visited_countries");
+
   let countries = [];
   result.rows.forEach((country) => {
     countries.push(country.country_code);
   });
-  console.log('rows', result.rows);
+  return countries;
+}
+
+app.get("/", async (req, res) => {
+  const countries = await checkVisitedCountry();
+
   res.render("index.ejs", { countries, total: countries.length })
-  db.end();
+});
+
+app.post("/add", async (req, res) => {
+  const input = req.body["country"];
+
+  const result = await db.query("select country_code from countries where country_name = $1", [ input ]);
+  
+  if (result.rows.length !== 0) {
+    const data = result.rows[0];
+    const countryCode = data.country_code;
+    
+    await db.query("insert into visited_countries (country_code) values ($1)", [ countryCode ]);
+    res.redirect("/");
+  }
 });
 
 app.listen(port, () => {
